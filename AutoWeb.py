0ec28da0d405596a4416f90170fb50704c6291db
GastonUC <Gacox.magallanes@gmail.com>
2020-10-02 02:04:33 -0300

    Se añade método para contar imágenes

--- AutoWeb.py
+++ AutoWeb.py
@@ -20,60 +20,65 @@ url = input("Ingrese la url: ")
 driver = webdriver.Chrome(PATH)
 driver.get(url)
 
-def getModelo():
-    try:
-        modelo = WebDriverWait(driver, 5).until(
-            EC.presence_of_element_located((By.CLASS_NAME, "section-title"))
-        )
-        print("Modelo: "+modelo.text)
-    except:
-        print("No se ha encontrado el modelo del auto ingresado.")
+#def getModelo():
+try:
+    modelo = WebDriverWait(driver, 5).until(
+        EC.presence_of_element_located((By.CLASS_NAME, "section-title"))
+    )
+    print("Modelo: "+modelo.text)
+except:
+    print("No se ha encontrado el modelo del auto ingresado.")
 
-def getPrecio():
-    try:
-        price = WebDriverWait(driver, 5).until(
-            EC.presence_of_element_located((By.CLASS_NAME, "price"))
-        )
-        print("Precio: "+price.text)
-    except:
-        print("No se ha encontrado el precio del modelo ingresado.")
+#def getPrecio():
+try:
+    price = WebDriverWait(driver, 5).until(
+        EC.presence_of_element_located((By.CLASS_NAME, "price"))
+    )
+    print("Precio: "+price.text)
+except:
+    print("No se ha encontrado el precio del modelo ingresado.")
 
-def getDescripcion():
-    try:
-        descripcion = WebDriverWait(driver, 5).until(
-            EC.presence_of_element_located((By.XPATH, "//div[@class='col-md-12']/p"))
-            #Otherwise /html/body/div/div/div/div/div/div/main/article/div/div/p
-        )
-        print("Descripcion: "+descripcion.text)
-    except:
-        print("No se ha encontrado la descripcion del modelo ingresado.")
+#def getDescripcion():
+try:
+    descripcion = WebDriverWait(driver, 5).until(
+        EC.presence_of_element_located((By.XPATH, "//div[@class='col-md-12']/p"))
+        #Otherwise /html/body/div/div/div/div/div/div/main/article/div/div/p
+    )
+    print("Descripcion: "+descripcion.text)
+except:
+    print("No se ha encontrado la descripcion del modelo ingresado.")
 
-def otraWea():
-    try:
-        img = WebDriverWait(driver, 5).until(
-            EC.presence_of_element_located((By.XPATH, "//li[@data-slide='2']/a"))
-        )
-        actions = ActionChains(driver)
-        actions.move_to_element(img)
-        actions.context_click(img)
-        actions.perform()
-        for x in range(7):
-            pyautogui.press('down')
-        pyautogui.press('enter')
-        time.sleep(1)
+# count = driver.find_elements(By.XPATH, "//li[@data-slide='1']/a")
+count = driver.find_elements(By.XPATH, "//li[@class='slide']/a")
+asd = len(count)
+print(asd)
 
-        fileName = modelo.text #arreglar
-        fName = ""
+#def obtencionImagenes():
+# try:
+#     img = WebDriverWait(driver, 5).until(
+#         EC.presence_of_element_located((By.XPATH, "//li[@data-slide='2']/a"))
+#     )
+#     actions = ActionChains(driver)
+#     actions.move_to_element(img)
+#     actions.context_click(img)
+#     actions.perform()
+#     for x in range(7):
+#         pyautogui.press('down')
+#     pyautogui.press('enter')
+#     time.sleep(1)
 
-        for character in fileName:
-            if character.isalnum():
-                fName += character
-        pyautogui.write(fName)
-        pyautogui.press('enter')
+#     fileName = modelo.text #arreglar
+#     fName = ""
 
-        time.sleep(1) #Posiblemente falle con un nuevo link
-    except:
-        print("link no encontrado")
-        driver.quit()
+#     for character in fileName:
+#         if character.isalnum():
+#             fName += character
+#     pyautogui.write(fName)
+#     pyautogui.press('enter')
+
+#     time.sleep(1) #Posiblemente falle con un nuevo link
+# except:
+#     print("link no encontrado")
+#     driver.quit()
 
 driver.quit()
