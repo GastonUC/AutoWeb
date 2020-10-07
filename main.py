1eb98fe1c82f568d0dae42a9d5daf60f0866d364
GastonUC <Gacox.magallanes@gmail.com>
2020-10-07 12:32:31 -0300

    Agrego método a incorporar para primer sorteo de dos elementos

--- main.py
+++ main.py
@@ -1,10 +1,29 @@
 # import AutoWeb
+import math
 
-print("Another stuff to add")
 
-var = 6
+var = 8
+pos = []
 
 for x in range(var):
-    print(x+1)
-else:
-    pass
+    pos.append(x+1) #Llenando la list con la cantidad de valores entregados por "var"
+    # print(pos)
+
+print("Array completo: ") 
+print(pos) #Muestra el contenido del array
+
+med = math.trunc(len(pos)/2)
+print(med) #Muestra la mitad del array, en caso de ser float lo aproxima.
+
+if(var == 4) or (var == 6) or (var == 8): #Solución mala al primer cambio de los items.
+    med = med - 1
+
+print(med)
+
+posIni = pos[0]
+posMed = pos[med]
+pos[med] = posIni   #Cambio '1' de posición 0 a posición del medio, guardando el número del medio en variable "posMed" para luego llevarlo a posición 0.
+pos[0] = posMed
+
+print("Array sorted: ")
+print(pos)
