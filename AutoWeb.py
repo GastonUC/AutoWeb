a530ae2534e0d3d778ea57f239edafb2480e3dff
GastonUC <Gacox.magallanes@gmail.com>
2021-08-23 03:43:29 -0700

    Obtencion de imagenes funcionando correctamente

--- AutoWeb.py
+++ AutoWeb.py
@@ -1,4 +1,4 @@
-#Este script se encargará de abrir una página por si solo y extraer cierta información.
+#Este script se encargará de abrir una página por si solo y extraer información especifica.
 
 import os
 import time #Just for the sleep, then maybe erase it
@@ -12,11 +12,15 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.action_chains import ActionChains
 
+#Cambiar de acuerdo al PATH actual del archivo "chromedriver"
 PATH = "/Users/Win10Pro/Documents/AutoWeb/chromedriver"
 
+#El usuario debe ingresar la url a utilizar.
 url = input("Ingrese la url: ")
 
+#Reconoce el archivo del driver y lo almacena en la variable 'driver'
 driver = webdriver.Chrome(PATH)
+#Ejecuta el driver con la url entregada por el usuario anteriormente.
 driver.get(url)
 
 #def getModelo():
@@ -47,69 +51,65 @@ try:
 except:
     print("No se ha encontrado la descripcion del modelo ingresado.")
 
-# count = driver.find_elements(By.XPATH, "//li[@data-slide='1']/a")
+#Se realiza una busqueda para todas las imagenes que tenga la clase 'slide' dentro de la pagina.
 count = driver.find_elements(By.XPATH, "//li[@class='slide']/a")
+#Guarda la cantidad de imagenes encontradas.
 contador = len(count)
-print(contador)
-
-#def obtencionImagenes():
-# try:
-#     img = WebDriverWait(driver, 5).until(
-#         EC.presence_of_element_located((By.XPATH, "//li[@data-slide='1']/a"))
-#     )
-#     actions = ActionChains(driver)
-#     actions.move_to_element(img)
-#     actions.context_click(img)
-#     actions.perform()
-#     for x in range(7):
-#         pyautogui.press('down')
-#     pyautogui.press('enter')
-#     time.sleep(1)
-
-#     fileName = modelo.text #arreglar
-#     fName = ""
-
-#     for character in fileName:
-#         if character.isalnum():
-#             fName += character
-#     pyautogui.write(fName)
-#     pyautogui.press('enter')
-
-#     time.sleep(1) #Posiblemente falle con un nuevo link
-# except:
-#     print("link no encontrado")
-#     driver.quit()
+#print(contador) #Para motivos Debug.
 
 
-if(contador == 3):
-    ar = [3,1,2]
-elif(contador == 4):
-    ar = [4,1,2,3]
-elif(contador == 5):
-    ar = [4,5,1,2,3]
-elif(contador == 6):
-    ar = [5,6,1,2,3,4]
-elif(contador == 7):
-    ar = [5,6,7,1,2,3,4]
-elif(contador == 8):
-    ar = [6,7,8,1,2,3,4,5]
-elif(contador == 9):
-    ar = [6,7,8,9,1,2,3,4,5]
-else:
-    print("Error, please check the code.")
+# if(contador == 3):
+#     ar = [3,1,2]
+# elif(contador == 4):
+#     ar = [4,1,2,3]
+# elif(contador == 5):
+#     ar = [4,5,1,2,3]
+# elif(contador == 6):
+#     ar = [5,6,1,2,3,4]
+# elif(contador == 7):
+#     ar = [5,6,7,1,2,3,4]
+# elif(contador == 8):
+#     ar = [6,7,8,1,2,3,4,5]
+# elif(contador == 9):
+#     ar = [6,7,8,9,1,2,3,4,5]
+# else:
+#     print("Error, please check the code.")
 
 try:
     for x in range(contador):
-        img = WebDriverWait(driver, 10).until(
-            EC.presence_of_element_located((By.XPATH, "//li[@data-slide='"+str(ar[x])+"']/img"))
-        )
+        try:
+            img = WebDriverWait(driver, 10).until(
+                #EC.presence_of_element_located((By.XPATH, "//li[@data-slide='"+str(ar[x])+"']/img"))
+                #Realiza busqueda de Imagen en Miniatura.
+                EC.presence_of_element_located((By.XPATH, "//li[@data-slide='"+str(x+1)+"']/img"))
+            )
+        except:
+            print("Imagen Miniatura no encontrada.")
+            driver.quit()
+
         actions = ActionChains(driver)
         actions.move_to_element(img)
-        actions.context_click(img)
+        actions.click(img)
         actions.perform()
+        actions.reset_actions()
+
+        try:
+            img2 = WebDriverWait(driver, 10).until(
+                #Realiza busqueda de Imagen en Grande.
+                EC.presence_of_element_located((By.XPATH, "//li[@data-slide='"+str(x+1)+"']/a[@class='single-image fancybox']/img"))
+            )
+        except:
+            print("Imagen Normal no encontrada.")
+            driver.quit()
+
+        actions.move_to_element(img2)
+        actions.context_click(img2)
+        actions.perform()
+        actions.reset_actions()
+        time.sleep(1)
         # for z in range(7):
         #     pyautogui.press('down')
-        pyautogui.press('down', presses=7) #presses equivale al total de veces que presionara la KEY
+        pyautogui.press('down', presses=8) #presses equivale al total de veces que presionara la KEY
         pyautogui.press('enter')
         time.sleep(1) #Espera a que cargue el pop-up
 
@@ -123,10 +123,10 @@ try:
         pyautogui.press('enter')
 
         time.sleep(1)
-        print("Imagen "+str(x+1)+" guardada.")
+        print("Imagen N"+str(x+1)+" guardada.")
 except:
     print("Elemento no encontrado")
     driver.quit()
         
-
+print("Proceso finalizado con exito.")
 driver.quit()
