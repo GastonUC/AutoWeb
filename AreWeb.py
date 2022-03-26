2a2b59b0307cebf6499be677b6d361a95b5699f9
Gaston Umaña <Gacox@Gastons-MacBook-Pro.local>
2022-03-25 23:31:46 -0300

    Corrección de funcionamiento script AreWeb, obtención de links logrado

--- AreWeb.py
+++ AreWeb.py
@@ -12,7 +12,9 @@ from selenium.webdriver.support import expected_conditions as EC
 driver = webdriver.Safari()
 #link: https://www.propiedadesarecheta.cl/propiedades/propiedad-calle-guillermo-tell/
 
-driver.get("https://www.propiedadesarecheta.cl/propiedades/propiedad-calle-guillermo-tell/")
+# driver.get("https://www.propiedadesarecheta.cl/propiedades/calle-los-maderos-altos-del-bosque/")
+driver.get("https://www.propiedadesarecheta.cl/propiedades/calle-zenteno-cerro-primavera/")
+# driver.get("https://www.propiedadesarecheta.cl/propiedades/depto-4o-piso-condominio-san-ignacio-1/")
 
 # try:
 #     img = driver.find_element(By.XPATH, "//div[@class='col-md-3 col-sm-6']/a")
@@ -31,9 +33,8 @@ try:
     close_btn = WebDriverWait(driver, 10).until(
         EC.presence_of_element_located((By.XPATH, "//span/i[@class='spu-icon spu-icon-close']"))
     )
-    print("Close btn encontrado.")
-except:
-    print("Close btn no encontrado.")
+except Exception as e:
+    print("Close btn no encontrado." + e)
     driver.quit()
 
 actions1 = ActionChains(driver)
@@ -41,7 +42,6 @@ actions1.move_to_element(close_btn)
 actions1.click(close_btn)
 actions1.perform()
 actions1.reset_actions()
-print("close btn apretado.")
 
 # name = driver.find_element(By.XPATH, "//div[@class='col-md-3 col-sm-6']/a/img")
 name = driver.find_element(By.XPATH, "//div[@class='page-title']/h1")
@@ -53,13 +53,27 @@ print(price.text)
 descripcion = driver.find_element(By.XPATH, "//div[@class='block-wrap']/div[@class='block-content-wrap']/p")
 print(descripcion.text)
 
-# img1 = driver.find_element(By.XPATH, "//div[@class='slick-track']/div[@class='slick-slide']/img")
+def Image_counter():
+    img = driver.find_elements(By.XPATH, "//div[@class='slick-slide']/img")
+    Img_count = len(img) #Contador
+    print(f"Contador de imagenes: {Img_count}") #Imprime el total de imagenes que hay
+    return Img_count + 1
+
+# print(Image_counter()) debugging purposes
+
 try:
-    img1 = driver.find_element(By.XPATH, "//img[@class='img-fluid']")
-    print(img1.text)
+    for x in range(Image_counter()):
+        try:
+            img1 = driver.find_element(By.XPATH, "//div[@data-slick-index='" + str(x) + "']/img[@class='img-fluid']"). get_attribute("src") #Obtiene el link de las imagenes
+            print(img1)
+        except Exception as e:
+            print(f"Element not found, {str(e)}")
+            driver.close()
 except Exception as e:
-    print("Element not found " + e)
+    print(f"Error trying to get Images: {e}")
     driver.close()
+
+driver.quit()
 
 # img_link = driver.find_element(By.XPATH, "//div/div[@data-slick-index='0']/img")
 # print(img_link.text)
@@ -80,3 +94,5 @@ except Exception as e:
     
 # makes the browser wait if it can't find an element
 # browser.implicitly_wait(10)
+
+# Recordar que tengo dos soluciones, agregar un numero al total de links obtenidos, y luego es agregar una segunda busqueda para la imagen faltante que es de index 0
