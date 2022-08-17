9ff796a4d008b9d7f8d977c1f1fbfde6251447c5
Gaston Umaña <Gacox.magallanes@gmail.com>
2022-08-17 17:45:52 -0400

    Se incorpora un gitignore y CardenaWeb

--- AreWeb.py
+++ AreWeb.py
@@ -15,55 +15,67 @@ driver = webdriver.Safari()
 
 #Counter for total images
 def Image_counter():
-    img = driver.find_elements(By.XPATH, "//div[@class='slick-slide']/img")
+    img = driver.find_elements(By.XPATH, "//div[@class='col-md-3 col-sm-6']")
     Img_count = len(img)
     return Img_count + 1
 
+print(Image_counter())
+# url = driver.find_element(By.XPATH, "//a[@class='houzez-trigger-popup-slider-js swipebox hover-effect']").get_attribute("data-toggle¿")
+# url1 = driver.find_element(By.XPATH, "//a[@data-slider-no='1']/img")
+# url1.value_of_css_property("src")
+# print(url)
 
-date = datetime.datetime.now().strftime("%d.%m.%y")
-try:
-    os.mkdir(f"{date}")
-    open(f"{date}/Texto Casas {date}.txt", "x")
-except FileExistsError:
-    print("Directory already exist.")
-except OSError as e:
-    print(f"Error trying to open file. {e}")
-    driver.close()
+print(driver.execute_script("return window.getComputedStyle(document.querySelector('a.1'),':before').getPropertyValue('content')").strip)
 
-input_links = list(map(str, input("Ingrese las url: ").split()))
-links = len(input_links)
-#Loop for all the Urls
-for x in range(links):
-    print(f"URL: {input_links[x]}")
-    driver.get(input_links[x])
+#https://www.propiedadesarecheta.cl/propiedades/calle-manuel-tangacis/
 
-    name = driver.find_element(By.XPATH, "//div[@class='page-title']/h1")
-    price = driver.find_element(By.XPATH, "//ul[@class='item-price-wrap hide-on-list']/li")
-    descripcion = driver.find_element(By.XPATH, "//div[@class='block-wrap']/div[@class='block-content-wrap']/p")
 
-    with open(f"{date}/Texto Casas {date}.txt", "a") as txt:
-        txt.write(f"Nombre: {name.text} \n Precio: {price.text} \n Descripción: {descripcion.text} \n\n")
 
-    os.mkdir(f"{date}/Propiedad {str(x+1)}")
 
-    try:
-        for y in range(Image_counter()):
-            try:
-                #Gets Url from images
-                url = driver.find_element(By.XPATH, "//div[@data-slick-index='" + str(y) + "']/img[@class='img-fluid']").get_attribute("src")
-                print(f"Link de la imagen {url}")
+# date = datetime.datetime.now().strftime("%d.%m.%y")
+# try:
+#     os.mkdir(f"{date}")
+#     open(f"{date}/Texto Casas {date}.txt", "x")
+# except FileExistsError:
+#     print("Directory already exist.")
+# except OSError as e:
+#     print(f"Error trying to open file. {e}")
+#     driver.close()
 
-                #fetch the content from the url and returns it in bytes. (Binary Response content)
-                response = requests.get(url).content
-                file = open(f"{date}/Propiedad {str(x+1)}/{name.text} {str(y+1)}.jpg", "wb")
-                file.write(response)
-                file.close
-                print(f"Imágen {y+1} guardada con éxito")
-            except Exception as e:
-                print(f"Element not found, {e}")
-                driver.close()
-    except Exception as e:
-        print(f"Error trying to get Images, {e}")
-        driver.close()
-print("Finalizado con éxito!")
+# input_links = list(map(str, input("Ingrese las url: ").split()))
+# links = len(input_links)
+# #Loop for all the Urls
+# for x in range(links):
+#     print(f"URL: {input_links[x]}")
+#     driver.get(input_links[x])
+
+#     name = driver.find_element(By.XPATH, "//div[@class='page-title']/h1")
+#     price = driver.find_element(By.XPATH, "//ul[@class='item-price-wrap hide-on-list']/li")
+#     descripcion = driver.find_element(By.XPATH, "//div[@class='block-wrap']/div[@class='block-content-wrap']/p")
+
+#     with open(f"{date}/Texto Casas {date}.txt", "a") as txt:
+#         txt.write(f"Nombre: {name.text} \n Precio: {price.text} \n Descripción: {descripcion.text} \n\n")
+
+#     os.mkdir(f"{date}/Propiedad {str(x+1)}")
+
+#     try:
+#         for y in range(Image_counter()):
+#             try:
+#                 #Gets Url from images
+#                 url = driver.find_element(By.XPATH, "//a[@data-slider-no='" + str(y) + "']/img").get_attribute("src")
+#                 print(f"Link de la imagen {url}")
+
+#                 #fetch the content from the url and returns it in bytes. (Binary Response content)
+#                 response = requests.get(url).content
+#                 file = open(f"{date}/Propiedad {str(x+1)}/{name.text} {str(y+1)}.jpg", "wb")
+#                 file.write(response)
+#                 file.close
+#                 print(f"Imágen {y+1} guardada con éxito")
+#             except Exception as e:
+#                 print(f"Element not found, {e}")
+#                 driver.close()
+#     except Exception as e:
+#         print(f"Error trying to get Images, {e}")
+#         driver.close()
+# print("Finalizado con éxito!")
 driver.quit()
